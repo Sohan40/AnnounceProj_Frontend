fa72b682627f505b48ff040bbfe655971346ace6
pranaysuhas1214 <suhaspranay777@gmail.com>
2023-07-02 22:53:59 +0530

    navbar functionality
    navbar functionality done responsive

---
+++ Project/src/components/Lists.jsx
@@ -0,0 +1,10 @@
+function Lists() {
+    return (
+        <ul className="navLinks text-white" id="navLinksId">
+            <li className="listCss"> <a>Clubs</a> </li>
+            <li className="listCss"> <a>About Us</a> </li>
+            <li className="listCss"> <button className="pb-2 signBtn">Sign Up</button> </li>
+        </ul>
+    );
+}
+export default Lists;
--- Project/src/components/Navbar.jsx
+++ Project/src/components/Navbar.jsx
@@ -1,20 +1,39 @@
 import { useEffect } from 'react'
+import { useState } from 'react'
 import './Navbar.css'
+import Lists from './Lists';
 function Navbar() {
+    const handleResize = () => {
+        if(window.innerWidth >=750)
+        {
+            setShowMenu(false);
+        }
+        setMinWidth(window.innerWidth < 750);
+      };
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+      }, []);
+    function expand()
+    {
+        setShowMenu(!showMenu);
+    }
+    const [showMenu,setShowMenu] = useState(false);
+    const [minWidth,setMinWidth] =useState(window.innerWidth<750);
     return (
         <nav className="largeScreenNav" style={{backgroundColor: "#090B0C"}}>
             <div className="navbar m-0">
                 <h1 className="text-white">L O G O</h1>
+                {minWidth?
                 <button className="toggleBtn text-center p-0" id="toggleBtnId">
-                    <i id="bars" className="fa-solid fa-bars m-0 mt-auto" style={{color: "#DB435C", fontSize: "40px"}}></i>
-                    <i id="cross" className="fa-solid fa-xmark m-0 mt-auto " style={{color: "#DB435C" ,fontSize: "40px"}}></i>
+                    {!showMenu?
+                    (<i onClick={expand} id="bars" className="fa-solid fa-bars m-0 mt-auto" style={{color: "#DB435C", fontSize: "40px"}}></i>)
+                    :
+                    (<i onClick={expand} id="cross" className="fa-solid fa-xmark m-0 mt-auto " style={{color: "#DB435C" ,fontSize: "40px"}}></i>)
+                    }
                 </button>
+                :null}
             </div>
-            <ul className="navLinks text-white" id="navLinksId">
-                <li className="listCss"> <a>Clubs</a> </li>
-                <li className="listCss"> <a>About Us</a> </li>
-                <li className="listCss"> <button className="pb-2 signBtn">Sign Up</button> </li>
-            </ul>
+            {(showMenu|| !minWidth)?<Lists/>:null}
         </nav>
     )
 }
