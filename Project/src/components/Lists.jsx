5e293e3732d2b48e006d76e32273331d5f764d5b
Sohan <sreecharansohanoffl@gmail.com>
2023-07-03 01:14:54 +0530

    Login commit

--- Project/src/components/Lists.jsx
+++ Project/src/components/Lists.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 function Lists() {
     return (
         <ul className="navLinks text-white" id="navLinksId">
-            <li className="listCss"> <a>Clubs</a> </li>
-            <li className="listCss"> <a>About Us</a> </li>
-            <li className="listCss"> <button className="pb-2 signBtn">Sign Up</button> </li>
+            <li className="listCss"> <Link>Clubs</Link> </li>
+            <li className="listCss"> <Link>About Us</Link> </li>
+            <li className="listCss"> <Link className="pb-2 signBtn" to="/login" >Login</Link> </li>
+            <li className="listCss"> <Link className="pb-2 signBtn" >Sign Up</Link> </li>
         </ul>
     );
 }
