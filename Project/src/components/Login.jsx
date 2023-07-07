c7337f9f6a19a692b7e72e992d6042c59b38bcf9
pranaysuhas1214 <suhaspranay777@gmail.com>
2023-07-07 14:05:38 +0530

    Login Page Validation

--- Project/src/components/Login.jsx
+++ Project/src/components/Login.jsx
@@ -1,5 +1,8 @@
 import "./Login.css"
+import {useForm} from "react-hook-form";
 export default function Login() {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const onSubmit = (data) => console.log(data);
     return (
         <div className="login">
             <div className="container">
@@ -7,15 +10,21 @@ export default function Login() {
             <div className="col-md-6">
                 <div className="login-box">
                     <h2>Login</h2>
-                    <form className="w-100">
-                        <div className="mb-3">
-                            <input type="text" className="form-control" name="username" id="username" placeholder="Your username"/>
+                    <form className="w-100" onSubmit={handleSubmit(onSubmit)}>
+                        <div className="mb-2">
+                            <input  {...register("username", { required: "username required" })} 
+                            style={{backgroundColor: errors.username?.type === 'required' && 'rgba(255, 110,110,0.553)'}}
+                            aria-invalid={errors.firstName ? "true" : "false"} 
+                            type="text" className="form-control" name="username" id="username" placeholder="Your username"/>
+                            {errors.username?.type === 'required' ?  <small className="valid" ><i className="fa-solid fa-triangle-exclamation"></i> username is required</small> :<div style={{height:"22px"}}></div> }
                             
                         </div>
 
-                        <div className="mb-3">
-                            <input type="text" className="form-control" name="password" id="password" placeholder="Your password"/>
-                            
+                        <div className="mb-2">
+                            <input {...register("password", { required: true })} 
+                            style={{backgroundColor: errors.password?.type === 'required' && 'rgba(255, 110,110,0.553)'}}
+                            type="text" className="form-control" name="password" id="password" placeholder="Your password"/>
+                            {errors.password?.type === 'required' ? <small className="valid"><i className="fa-solid fa-triangle-exclamation"></i> password is required</small>:<div style={{height:"22px"}}></div>}
                         </div>
                     
                         <input type="submit" value="Login"/>
