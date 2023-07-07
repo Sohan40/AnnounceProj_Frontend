e031e971915111ed8e8316206cdae2fc4059c602
Rutvik Pradyumna <pradyumnarutvik@gmail.com>
2023-07-08 01:52:53 +0530

    home1.1

--- Project/src/components/Home.jsx
+++ Project/src/components/Home.jsx
@@ -5,12 +5,18 @@ function Home() {
         <div className='home'>
             <Navbar/>
             <div className='container-fluid' style={{flex:'1',display: 'flex', flexDirection: 'column'}}>
-                <div className='row' style={{ flex: '1' }}>
+                <div className='row smallDevice' style={{ flex: '1' }}>
                     <div className='col-12 col-md-6 content'>
-                        <h1>ihi</h1>
+                        <h1 className='homeHead'>A place for all announcements...</h1>
+                        <p className='text-white' style={{textAlign:'justify'}}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                        </p>
+                        <div className='signBtnContainer'>
+                            <button className='signBtn'>Sign Up</button>
+                            <button className='signBtn'>Sign In</button>
+                        </div>
                     </div>
-                    <div className='col-12 col-md-6 image'>
-                        <h1>image</h1>
+                    <div className='col-12 col-md-6 image' style={{padding:'0',margin:'0'}}>
                     </div>
                 </div>
             </div>
--- Project/src/components/Lists.jsx
+++ Project/src/components/Lists.jsx
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom";
 function Lists(props) {
     return (
         <ul className={`navLinks text-white${props.addClass?' active':null}`} id="navLinksId">
-            <li className="listCss topElement"> <Link>Clubs</Link> </li>
-            <li className="listCss"> <Link className="signBtn" to="/login" >Login</Link> </li>
-            <li className="listCss"> <Link className="signBtn" >Sign Up</Link> </li>
+            <li className="listCss"> <Link>Home</Link> </li>
+            <li className="listCss"> <Link>Clubs</Link> </li>
+            <li className="listCss"> <Link to="/login" >Login</Link> </li>
+            <li className="listCss"> <Link>Sign Up</Link> </li>
             <li className="listCss">
                 <span>
                     <Link className="profile">
