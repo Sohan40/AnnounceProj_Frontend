7e76305c0fae3018f6af25bac45e34da52104313
muramsettiAbhisai <muramsettias@gmail.com>
2023-07-25 13:11:29 +0530

    nothingg...
    just a commit

--- Project/src/components/App.jsx
+++ Project/src/components/App.jsx
@@ -5,7 +5,12 @@ import Register from "./loginRegister/Register"
 import Clubs from './Clubs'
 import Navbar  from "./Navbar"
 import ClubPage from "./clubPage/ClubPage"
+window.onbeforeunload=(event)=>{
+  event.preventDefault();
+  return "";
+}
 function App() {
+ 
   return (
     <>
      <Navbar/>
